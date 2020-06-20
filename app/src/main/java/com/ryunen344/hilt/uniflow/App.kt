d87836d57ff198718ede971dfa244b9acdbca525
RyuNen344 <s1100633@outlook.com>
2020-06-21 00:34:56 +0900

    Introduction dagger hilt

--- app/src/main/java/com/ryunen344/hilt/uniflow/App.kt
+++ app/src/main/java/com/ryunen344/hilt/uniflow/App.kt
@@ -1,14 +1,30 @@
 package com.ryunen344.hilt.uniflow
 
 import android.app.Application
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.components.ApplicationComponent
 import timber.log.LogcatTree
 import timber.log.Timber
 
+@HiltAndroidApp
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
         Timber.plant(LogcatTree())
+    }
+}
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class ApplicationModule {
+
+    @Provides
+    fun provideHash(): String {
+        return hashCode().toString()
     }
 }
--- feature/home/src/main/java/com/ryunen344/hilt/uniflow/home/HomeModule.kt
+++ feature/home/src/main/java/com/ryunen344/hilt/uniflow/home/HomeModule.kt
@@ -1,1 +1,11 @@
 package com.ryunen344.hilt.uniflow.home
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+
+@Module
+@InstallIn(FragmentComponent::class)
+abstract class HomeModule {
+
+}
